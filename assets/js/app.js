82862db78eb5208e3649105f5253ba4c4c2ad69e
Kos-Mos <ChuyM203@gmail.com>
2020-07-15 19:27:12 -0400

    added specific ID's in html/created basic variables in script

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+// Declaring Basic Variables
+const navbarEl = document.querySelector(`#navbar-container`);
+const cityContainerEl = document.querySelector(`#city-container`);
+const cityInfoEl = document.querySelector(`#city-info`);
+const testingCenterEl = document.querySelector(`#testing-center`);
+const mapContainerEl = document.querySelector(`#map-container`);
+const tipsContainerEl = document.querySelector(`#tips-container`);
