22c820c3e3b1c04b4218f52c78928fe8ae51f36b
SmithBWare <Smithwrestling89@gmail.com>
2020-07-19 12:07:36 -0400

    added ability to populate state specific data based on marker click

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -19,207 +19,258 @@ const states = 'al';
 const stateInfo = [
   {
     state: 'Alabama',
-    coords: [32.806671, -86.791130]
+    coords: [32.806671, -86.791130], 
+    abbreviation: 'al'
   },
   {
     state: 'Alaska',
-    coords: [61.370716, 152.404419]
+    coords: [61.370716, 152.404419], 
+    abbreviation: 'ak'
   },
   {
     state: 'Arizona',
-    coords: [33.729759, -111.431221]
+    coords: [33.729759, -111.431221], 
+    abbreviation: 'az'
   },
   {
     state: 'Arkansas',
-    coords: [34.969704, -92.373123]
+    coords: [34.969704, -92.373123], 
+    abbreviation: 'ak'
   },
   {
     state: 'California', 
-    coords: [36.116203, -119.681564]
+    coords: [36.116203, -119.681564], 
+    abbreviation: 'ca'
   },
   {
     state: 'Colorado',
-    coords: [39.059811, -105.311104]
+    coords: [39.059811, -105.311104], 
+    abbreviation: 'co'
   },
   {
     state: 'Connecticut',
-    coords: [41.597782, -72.755371]
+    coords: [41.597782, -72.755371], 
+    abbreviation: 'ct'
   },
   {
     state: 'Delaware',
-    coords: [39.318523, -75.507141]
+    coords: [39.318523, -75.507141], 
+    abbreviation: 'de'
   },
   {
     state: 'District Of Columbia',
-    coords: [38.897438, -77.026817]
+    coords: [38.897438, -77.026817], 
+    abbreviation: 'dc'
   },
   {
     state: 'Florida',
-    coords: [27.766279, -81.686783]
+    coords: [27.766279, -81.686783], 
+    abbreviation: 'fl'
   },
   {
     state: 'Georgia',
-    coords: [33.040619, -83.643074]
+    coords: [33.040619, -83.643074], 
+    abbreviation: 'ga'
   },
   {
     state: 'Hawaii',
-    coords: [21.094318, -157.498337]
+    coords: [21.094318, -157.498337], 
+    abbreviation: 'hi'
   },
   {
     state: 'Idaho', 
-    coords: [44.240459, -114.478828]
+    coords: [44.240459, -114.478828], 
+    abbreviation: 'id'
   },
   {
     state: 'Illinois', 
-    coords: [40.349457, -88.986137]
+    coords: [40.349457, -88.986137], 
+    abbreviation: 'il'
   },
   {
     state: 'Indiana', 
-    coords: [39.849426, -86.258278]
+    coords: [39.849426, -86.258278], 
+    abbreviation: 'in'
   },
   {
     state: 'Iowa', 
-    coords: [42.011539, -93.210526]
+    coords: [42.011539, -93.210526], 
+    abbreviation: 'ia'
   },
   {
     state: 'Kansas', 
-    coords: [38.526600, -96.726486]
+    coords: [38.526600, -96.726486], 
+    abbreviation: 'ks'
   },
   {
     state: 'Kentucky', 
-    coords: [37.668140, -84.670067]
+    coords: [37.668140, -84.670067], 
+    abbreviation: 'ky'
   },
   {
     state: 'Louisiana', 
-    coords: [31.169546, -91.867805]
+    coords: [31.169546, -91.867805], 
+    abbreviation: 'la'
   },
   {
     state: 'Maine', 
-    coords: [44.693947, -69.381927]
+    coords: [44.693947, -69.381927], 
+    abbreviation: 'me'
   },
   {
     state: 'Maryland', 
-    coords: [39.063946, -76.802101]
+    coords: [39.063946, -76.802101], 
+    abbreviation: 'md'
   },
   {
     state: 'Massachusetts', 
-    coords: [42.230171, -71.530106]
+    coords: [42.230171, -71.530106], 
+    abbreviation: 'ma'
   },
   {
     state: 'Michigan', 
-    coords: [43.326618, -84.536095]
+    coords: [43.326618, -84.536095], 
+    abbreviation: 'mi'
   },
   {
     state: 'Minnesota', 
-    coords: [45.694454, -93.900192]
+    coords: [45.694454, -93.900192], 
+    abbreviation: 'mn'
   },
   {
     state: 'Mississippi', 
-    coords: [32.741646, -89.678696]
+    coords: [32.741646, -89.678696], 
+    abbreviation: 'ms'
   },
   {
     state: 'Missouri', 
-    coords: [38.456085, -92.288368]
+    coords: [38.456085, -92.288368], 
+    abbreviation: 'mo'
   },
   {
     state: 'Montana', 
-    coords: [46.921925, -110.454353]
+    coords: [46.921925, -110.454353], 
+    abbreviation: 'mt'
   },
   {
     state: 'Nebraska', 
-    coords: [41.125370, -98.268082]
+    coords: [41.125370, -98.268082], 
+    abbreviation: 'ne'
   },
   {
     state: 'Nevada', 
-    coords: [38.313515, -117.055374]
+    coords: [38.313515, -117.055374], 
+    abbreviation: 'nv'
   },
   {
     state: 'New Hampshire', 
-    coords: [43.452492, -71.563896]
+    coords: [43.452492, -71.563896], 
+    abbreviation: 'nh'
   },
   {
     state: 'New Jersey', 
-    coords: [40.298904, -74.521011]
+    coords: [40.298904, -74.521011], 
+    abbreviation: 'nj'
   },
   {
     state: 'New Mexico', 
-    coords: [34.840515, -106.248482]
+    coords: [34.840515, -106.248482], 
+    abbreviation: 'nm'
   },
   {
     state: 'New York', 
-    coords: [42.165726, -74.948051]
+    coords: [42.165726, -74.948051], 
+    abbreviation: 'ny'
   },
   {
     state: 'North Carolina', 
-    coords: [35.630066, -79.806419]
+    coords: [35.630066, -79.806419], 
+    abbreviation: 'nc'
   },
   {
     state: 'North Dakota', 
-    coords: [47.528912, -99.784012]
+    coords: [47.528912, -99.784012], 
+    abbreviation: 'nd'
   },
   {
     state: 'Ohio', 
-    coords: [40.388783, -82.764915]
+    coords: [40.388783, -82.764915], 
+    abbreviation: 'oh'
   },
   {
     state: 'Oklahoma', 
-    coords: [35.565342, -96.928917]
+    coords: [35.565342, -96.928917], 
+    abbreviation: 'ok'
   },
   {
     state: 'Oregon', 
-    coords: [44.572021, -122.070938]
+    coords: [44.572021, -122.070938], 
+    abbreviation: 'or'
   },
   {
     state: 'Pennsylvania', 
-    coords: [40.590752, -77.209755]
+    coords: [40.590752, -77.209755], 
+    abbreviation: 'pa'
   },
   {
-    state: 'RhodeIsland', 
-    coords: [41.680893, -71.511780]
+    state: 'Rhode Island', 
+    coords: [41.680893, -71.511780], 
+    abbreviation: 'ri'
   },
   {
-    state: 'SouthCarolina', 
-    coords: [33.856892, -80.945007]
+    state: 'South Carolina', 
+    coords: [33.856892, -80.945007], 
+    abbreviation: 'sc'
   },
   {
-    state: 'SouthDakota', 
-    coords: [44.299782, -99.438828]
+    state: 'South Dakota', 
+    coords: [44.299782, -99.438828], 
+    abbreviation: 'sd'
   },
   {
     state: 'Tennessee', 
-    coords: [35.747845, -86.692345]
+    coords: [35.747845, -86.692345], 
+    abbreviation: 'tn'
   },
   {
     state: 'Texas', 
-    coords: [31.054487, -97.563461]
+    coords: [31.054487, -97.563461], 
+    abbreviation: 'tx'
   },
   {
     state: 'Utah', 
-    coords: [40.150032, -111.862434]
+    coords: [40.150032, -111.862434], 
+    abbreviation: 'ut'
   },
   {
     state: 'Vermont', 
-    coords: [44.045876, -72.710686]
+    coords: [44.045876, -72.710686], 
+    abbreviation: 'vt'
   },
   {
     state: 'Virginia', 
-    coords: [37.769337, -78.169968]
+    coords: [37.769337, -78.169968], 
+    abbreviation: 'va'
   },
   {
     state: 'Washington', 
-    coords: [47.400902, -121.490494]
+    coords: [47.400902, -121.490494], 
+    abbreviation: 'wa'
   },
   {
     state: 'West Virginia', 
-    coords: [38.491226, -80.954453]
+    coords: [38.491226, -80.954453], 
+    abbreviation: 'wv'
   },
   {
     state: 'Wisconsin', 
-    coords: [44.268543, -89.616508]
+    coords: [44.268543, -89.616508], 
+    abbreviation: 'wi'
   },
   {
     state: 'Wyoming', 
-    coords: [42.755966, -107.302490]
+    coords: [42.755966, -107.302490], 
+    abbreviation: 'wy'
   }
 ];
 
@@ -230,12 +281,12 @@ let map = L.map(mapContainerEl).setView([37.0902, -95.7129], 4);
 let markers = [];
 
 
-async function covidData() {
+async function covidData(state) {
   // state Specific data
   try {
     // Fetch Data
     const stateData = await fetch(
-      `https://covidtracking.com/api/v1/states/${states}/current.json`
+      `https://covidtracking.com/api/v1/states/${state}/current.json`
     );
     // Format Data
     const formattedData = await stateData.json();
@@ -280,13 +331,19 @@ function interactiveMap() {
 
 function createMapMarkers(){
   for (let i = 0; i < stateInfo.length; i++) {
-    let marker = L.marker([stateInfo[i].coords[0], stateInfo[i].coords[1]]).addTo(map);
+    let marker = L.marker([stateInfo[i].coords[0], stateInfo[i].coords[1]], {title: stateInfo[i].abbreviation}).addTo(map);
   }
+}
+
+function populateStateInfo(event){
+  const stateAbbrev = event.originalEvent.srcElement.title
+  covidData(stateAbbrev);
 }
 
 interactiveMap();
 createMapMarkers();
-covidData();
+
+map.addEventListener("click", populateStateInfo);
 
 // function populateMapData {
 //     // Data to consider
